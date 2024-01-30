66a7dcff520461f05009fd22aaba7ab1f7e32d67
parasolya <o.shevchuk@gmail.com>
2024-01-30 20:31:55 +0200

    Build and deploy for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, "src") }],
-  },
-  css: {
-    // Вказати шлях до вашого головного CSS файлу
-    postcss: 'src/index.css',
-  },
-});
+    alias: [{ find: '@', replacement: path.resolve(__dirname, "src") }]
+  }
+})
